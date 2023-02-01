f2c8215a059c194ca4a6778d8012b84be8703888
Jose Domenech Leal <josesguay@gmail.com>
2023-02-01 10:17:27 +0000

    Fix clean-branches alias

--- .aliases.zsh
+++ .aliases.zsh
@@ -4,4 +4,4 @@ alias cdp='cd ~/Documents/personal_workspace'
 alias kctx='kubectx'
 alias k8s='kubectl'
 alias curltime="curl -w \"@$HOME/.curl-format\" -o /dev/null -s "
-alias clean-branches="git branch -D $(git branch -v | grep gone | awk '{print $1;}')"
+alias clean-branches="git branch -D \$(git branch -v | grep gone | awk '{print \$1;}')"
