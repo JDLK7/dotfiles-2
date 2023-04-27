b780ff631414a6251e4a64b98eaf2d174c29990d
Jose Domenech Leal <josesguay@gmail.com>
2023-04-27 10:07:47 +0200

    Improve aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -2,6 +2,7 @@ alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias cdw='cd ~/Documents/workspace'
 alias cdp='cd ~/Documents/personal_workspace'
 alias kctx='kubectx'
+alias kns='kubens'
 alias k8s='kubectl'
 alias curltime="curl -w \"@$HOME/.curl-format\" -o /dev/null -s "
-alias clean-branches="git branch -D \$(git branch -v | grep gone | awk '{print \$1;}')"
+alias clean-branches="git branch -D \$(git branch -v | grep -E '(gone|desaparecido)' | awk '{print \$1;}')"
